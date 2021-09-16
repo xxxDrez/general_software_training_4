1ea10190d49cfe712af80a4204024d2493c3543e
xxxDrez <xxxdrez@MacBook-Pro-xxxDrez.local>
2021-09-16 17:51:14 +0300

    Исправление ошибок

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,5 +1,3 @@
-setInterval(setClock, 1000)
-
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
